5d4a959ea996ecff8e922165712a4ac377d29d8d
DominikPyrek <pyrekdominik2025@gmail.com>
2025-07-11 12:53:09 +0200

    Tailwind and shadcn added

--- Frontend/ChessDuckVsGoose/src/App.tsx
+++ Frontend/ChessDuckVsGoose/src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [num, setNum] = useState(1);
+  function onClick() {
+    setNum(num + 1);
+  }
+  return (
+    <>
+      <Button className="bg-black" onClick={onClick}>
+        AAAAAAAa
+      </Button>
+      <p>{num}</p>
+    </>
+  );
 }
 
 export default App;
