939d42ffc24064d4037c203c0bc20c9667d498fa
DominikPyrek <pyrekdominik2025@gmail.com>
2025-07-14 13:06:15 +0200

    You can now move
    No validation yet

--- Frontend/ChessDucksVsGoose/src/pages/PlayChess.tsx
+++ Frontend/ChessDucksVsGoose/src/pages/PlayChess.tsx
@@ -1,13 +1,40 @@
 import { createInitialBoard, getPieceSymbol } from "@/chessUtils";
-import type { Board } from "@/types";
-import { useEffect, useState } from "react";
+import type { Board, ChessPiece } from "@/types";
+import { useState } from "react";
+type Position = [number, number];
 
 export default function PlayChess() {
-  const [chessBoard, setChessBoard] = useState<Board>();
+  const [chessBoard, setChessBoard] = useState<Board>(createInitialBoard());
+  const [draggedPiece, setDraggedPiece] = useState<{
+    piece: ChessPiece;
+    position: Position;
+  } | null>(null);
 
-  useEffect(() => {
-    setChessBoard(createInitialBoard);
-  }, []);
+  const handleDragStart = (row: number, col: number) => {
+    setDraggedPiece({
+      piece: chessBoard[row][col],
+      position: [row, col],
+    });
+  };
+
+  const handleDrop = (toRow: number, toCol: number) => {
+    if (!draggedPiece) return;
+
+    const [fromRow, fromCol] = draggedPiece.position;
+
+    setChessBoard((prev) => {
+      const newBoard = [...prev.map((row) => [...row])];
+      newBoard[toRow][toCol] = draggedPiece.piece;
+      newBoard[fromRow][fromCol] = null;
+      return newBoard;
+    });
+
+    setDraggedPiece(null);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
 
   if (chessBoard == undefined) {
     return <div>Loading</div>;
@@ -22,9 +49,15 @@ export default function PlayChess() {
               key={`${rowIndex}-${colIndex}`}
               className={`flex items-center justify-center w-[99px] h-[99px]
               ${(rowIndex + colIndex) % 2 === 0 ? "bg-white" : "bg-gray-500"}`}
+              onDrop={() => handleDrop(rowIndex, colIndex)}
+              onDragOver={handleDragOver}
             >
               {piece && (
-                <span className="text-2xl">
+                <span
+                  className="text-7xl"
+                  draggable="true"
+                  onDragStart={() => handleDragStart(rowIndex, colIndex)}
+                >
                   {getPieceSymbol(piece.type, piece.color)}
                 </span>
               )}
