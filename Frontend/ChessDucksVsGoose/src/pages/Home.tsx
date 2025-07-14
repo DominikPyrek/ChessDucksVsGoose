16cc276be2469d79a1dd9c9f8c8a1a23409b87c9
DominikPyrek <pyrekdominik2025@gmail.com>
2025-07-14 11:36:02 +0200

    Home done for now

--- Frontend/ChessDucksVsGoose/src/pages/Home.tsx
+++ Frontend/ChessDucksVsGoose/src/pages/Home.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { ArrowBigRight, LucideSparkle } from "lucide-react";
+import { ArrowBigRight, LucideSparkle, Bot } from "lucide-react";
 
 export default function Home() {
   const items = [
     {
       title: "Ready for Your Next Challenge?",
-      text: `Join millions of players worldwide in thrilling matches. 
-            Whether you're a beginner or grandmaster, there's always someone to play!`,
+      text: `Join millions of players worldwide in thrilling matches. Whether you're a beginner or grandmaster, there's always someone to play!`,
       key_points: [
         "4/7 matches with players at your level",
         "Zero ads, pure chess focus",
@@ -18,17 +17,30 @@ export default function Home() {
     },
     {
       title: "New to Chess?",
-      text: `✓ Learn rules in 5 minutes$
-            ✓ Basic checkmate patterns
-            ✓ Starter opening principles`,
+      text: `Chess is a timeless game of strategy that’s easy to learn but takes a lifetime to master. Whether you're just starting or need a quick refresher, we’ll help you grasp the basics fast.`,
+      key_points: [
+        "Learn rules in 5 minutes$",
+        "Basic checkmate patterns",
+        "Starter opening principles",
+      ],
       button_text: "Start Learning",
       button_icon: LucideSparkle,
+    },
+    {
+      title: "Challenge the Computer",
+      text: `Test your skills against AI opponents—from beginner to grandmaster level. No pressure, just pure chess!`,
+      key_points: [
+        "Adjustable difficulty – Play at your own pace.",
+        "Unlimited practice – No time limits, no stress.",
+      ],
+      button_text: "Play vs AI",
+      button_icon: Bot,
     },
   ];
 
   return (
     <div className="flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl mb-10 mt-20">Welcome To ChessDucksVsGoose</h1>
+      <h1 className="text-4xl mb-10 mt-10">Welcome To ChessDucksVsGoose</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 max-w-6xl w-full">
         {items.map((item) => (
           <Card className="p-4 m-4">
